e495e74bdcdfd4eda7ec1b7a6727a8c434c46167
Skivel <denisbuhrov@gmail.com>
2023-03-31 02:29:39 +0200

    add POST request for create user

--- API_Telegram_Registration/urls.py
+++ API_Telegram_Registration/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
+from login.views import (
+    login_view,
+    create_user
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', )
+    path('', login_view, name='login'),
+    path('user/create', create_user, name='user_create')
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from login.models import UserInfo
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+        else:
+            return render(request, 'none.html', {'error': 'Invalid credentials'})
+    else:
+        return render(request, 'login_page.html')
+
+
+@login_required
+def user_page(request):
+    pass
+
+
+def create_user(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user = UserInfo(user_name=data['name'], user_nickname=data['nickname'], user_email=data['email'],
+                        password=data['password'])
+        user.save()
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
