54fcc86f5358dab5e5c1b599b3422d57925aa915
Skivel <denisbuhrov@gmail.com>
2023-03-31 16:04:40 +0200

    Save user data and profile photo

--- API_Telegram_Registration/urls.py
+++ API_Telegram_Registration/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from login.views import (
     login_view,
+    logout_view,
     create_user,
     user_page
 )
@@ -10,6 +11,7 @@ from login.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
     path('user/create', create_user, name='user_create'),
     path('home', user_page, name='home')
 ]
--- login/models.py
+++ login/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class UserInfo(models.Model):
-    user_name = models.CharField(max_length=25, verbose_name='User name')
-    user_nickname = models.CharField(max_length=255, verbose_name='User nickname')
-    user_email = models.EmailField(verbose_name='User Email')
-    user_password = models.CharField(max_length=255, verbose_name='User password')
-    user_photo = models.FileField(upload_to='', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    user_name = models.CharField(max_length=25, verbose_name='User name', blank=True)
+    user_nickname = models.CharField(max_length=255, verbose_name='User nickname', blank=True)
+    user_email = models.EmailField(verbose_name='User Email', blank=True)
+    user_password = models.CharField(max_length=255, verbose_name='User password', blank=True)
+    user_photo = models.FileField(upload_to='users_image', blank=True)
 
     class Meta:
-        pass
+        verbose_name = 'User Info'
+        verbose_name_plural = 'Users Info'
+
+    def __str__(self):
+        return self.user_nickname
 
